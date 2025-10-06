1961e6d9d1cdabc7ab20ee4081b33c3b057400c7
veerbal1 <veerbalsingh1@gmail.com>
2025-10-06 12:24:18 +0530

    Update Anchor.toml for devnet configuration and add error handling in counter program

--- programs/counter/src/lib.rs
+++ programs/counter/src/lib.rs
@@ -2,15 +2,53 @@ use anchor_lang::prelude::*;
 
 declare_id!("EWV8Q3PXyaHQUBW3D7GexkUX2rb7v6xrQKwizpJKJPLF");
 
+#[error_code]
+pub enum ErrorCode {
+    #[msg("Cannot decrement below zero")]
+    CounterUnderflow,
+}
+
 #[program]
 pub mod counter {
     use super::*;
 
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+        let counter = &mut ctx.accounts.counter;
+        counter.count = 0;
+        Ok(())
+    }
+
+    pub fn increment(ctx: Context<Change>) -> Result<()> {
+        let counter = &mut ctx.accounts.counter;
+        counter.count += 1;
+        Ok(())
+    }
+
+    pub fn decrement(ctx: Context<Change>) -> Result<()> {
+        let counter = &mut ctx.accounts.counter;
+        require!(counter.count > 0, ErrorCode::CounterUnderflow);
+        counter.count -= 1;
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize<'info> {
+    #[account(init, payer = user, space = 8 + 8)]
+    pub counter: Account<'info, Counter>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct Change<'info> {
+    #[account(mut)]
+    pub counter: Account<'info, Counter>,
+    pub user: Signer<'info>,
+}
+
+#[account]
+pub struct Counter {
+    pub count: u64,
+}
